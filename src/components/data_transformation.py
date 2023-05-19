ac536ea89958bcde861cfb5f86ed41ad1fad7c78
Dev Shirishkar <devshirishkar@gmail.com>
2023-05-20 03:53:14 +0530

    Model Trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -63,6 +63,7 @@ class DataTransformation:
             
 
     def initiate_data_transformation(self,train_path,test_path):
+        
         try:
             train_df=pd.read_csv(train_path)
             test_df=pd.read_csv(test_path)
